c01967b7b809a2953bd591e10f39ff90d7882201
thiago-arend <thiago.arend@gmail.com>
2023-05-19 11:46:42 -0300

    cópia dos assets disponibilizados para dentro do projeto

---
+++ src/components/Deck.jsx
@@ -0,0 +1,8 @@
+export default function Deck() {
+  
+    return (
+      <>
+        
+      </>
+    )
+  }
---
+++ src/components/QuestionWrapper.jsx
@@ -0,0 +1,8 @@
+export default function QuestionWrapper() {
+  
+    return (
+      <>
+        
+      </>
+    )
+  }
