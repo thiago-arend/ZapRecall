c80cbf92d31e41860443d0d47885c4077be19100
thiago-arend <thiago.arend@gmail.com>
2023-05-20 11:59:04 -0300

    finalização estilo perguntas cards

--- src/components/QuestionWrapper.jsx
+++ src/components/QuestionWrapper.jsx
@@ -1,34 +1,38 @@
 import styled from "styled-components";
-import Answer from "./Answer";
-import Question from "./Question";
 import setaPlay from "../assets/seta_play.png";
+import setaVirar from "../assets/seta_virar.png";
 
 export default function QuestionWrapper(props) {
   const {question, answer} = props.question;
   const {number, setQuestsStatus} = props;
   
-    return (
-      <SCQuestWrapper>
-        {/*<Question question={question} />
-        <Answer answer={answer}
-    setQuestsStatus={setQuestsStatus} />*/}
-
+  return (
+    <>
+    <SCQuestWrapper>
       <span>Pergunta {(number + 1).toString()}</span>
       <img src={setaPlay} />
-      </SCQuestWrapper>
-    );
+    </SCQuestWrapper>
+    <SCQuestion>
+      <span>{question}</span>
+      <div>
+        <img src={setaVirar} />
+      </div>
+    </SCQuestion>
+    </>
+  );
 }
 
 const SCQuestWrapper = styled.div`
+
   margin-bottom: 20px;
   width: 100%;
   height: 65px;
   box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
   border-radius: 5px;
   background-color: #FFF;
-  padding: 0 20px;
+  padding: 0 12px;
 
-  display: flex;
+  display: none;
   justify-content: space-between;
   align-items: center;
 
@@ -41,6 +45,43 @@ const SCQuestWrapper = styled.div`
     font-style: normal;
     font-size: 16px;
     font-weight: 700;
+    color: #333;
+  }
+`;
+
+const SCQuestion = styled.div`
+  margin-bottom: 20px;
+  width: 100%;
+  height: 131px;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  background-color: #FFF;
+  padding: 0 12px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  div {
+    height: inherit;
+    padding-bottom: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+
+  img {
+    width: 30px;
+    height: 20px;
+  }
+
+  span {
+    margin-top: 12px;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
     color: #333;
   }
 `;
