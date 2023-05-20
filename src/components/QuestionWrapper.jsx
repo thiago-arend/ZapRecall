c66255bf8bdc31e314d03566c8193539e18a86f1
thiago-arend <thiago.arend@gmail.com>
2023-05-20 13:29:26 -0300

    finalização design cards resposta && finalização requisito de mostrar card de pergunta ao clicar

--- src/components/QuestionWrapper.jsx
+++ src/components/QuestionWrapper.jsx
@@ -1,23 +1,51 @@
 import styled from "styled-components";
 import setaPlay from "../assets/seta_play.png";
 import setaVirar from "../assets/seta_virar.png";
+import { useState } from "react";
 
 export default function QuestionWrapper(props) {
+  const [displayQuestion, setDisplayQuestion] = useState(false);
+  const [displayAnswer, setDisplayAnswer] = useState(false);
+
   const {question, answer} = props.question;
   const {number, setQuestsStatus} = props;
+
+  function resolveDisplayQuestion() {
+    if (!displayQuestion) {
+      setDisplayQuestion(true);
+      setDisplayAnswer(false);
+    }
+  }
+
+  function resolveDisplayAnswer() {
+    if (!displayAnswer) {
+      setDisplayQuestion(false);
+      setDisplayAnswer(true);
+    }
+  }
   
   return (
     <>
-    <SCQuestWrapper>
+    <SCQuestWrapper display={(!displayAnswer && !displayQuestion) ? "flex" : "none"} >
       <span>Pergunta {(number + 1).toString()}</span>
-      <img src={setaPlay} />
+      <img onClick={() => resolveDisplayQuestion()} src={setaPlay} />
     </SCQuestWrapper>
-    <SCQuestion>
+
+    <SCQuestion display={displayQuestion ? "flex" : "none"} >
       <span>{question}</span>
       <div>
-        <img src={setaVirar} />
+        <img onClick={() => resolveDisplayAnswer()} src={setaVirar} />
       </div>
     </SCQuestion>
+
+    <SCAnswer display={displayAnswer ? "flex" : "none"}>
+      <span>{answer}</span>
+      <div>
+        <button>Não lembrei</button>
+        <button>Quase não lembrei</button>
+        <button>Zap!</button>
+      </div>
+    </SCAnswer>
     </>
   );
 }
@@ -32,7 +60,7 @@ const SCQuestWrapper = styled.div`
   background-color: #FFF;
   padding: 0 12px;
 
-  display: none;
+  display: ${props => props.display};
   justify-content: space-between;
   align-items: center;
 
@@ -58,7 +86,7 @@ const SCQuestion = styled.div`
   background-color: #FFF;
   padding: 0 12px;
 
-  display: flex;
+  display: ${props => props.display};
   justify-content: space-between;
   align-items: flex-start;
 
@@ -84,4 +112,67 @@ const SCQuestion = styled.div`
     line-height: 22px;
     color: #333;
   }
+`;
+
+const SCAnswer = styled.div`
+  margin-bottom: 20px;
+  width: 100%;
+  height: 131px;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  background-color: #FFF;
+  padding: 0 12px;
+
+  display: ${props => props.display};
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  span {
+    margin-top: 12px;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    color: #333;
+  }
+
+  div {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 12px;
+  }
+
+  button {
+    width: 85px;
+    height: 37px;
+    font-family: 'Recursive';
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    border-radius: 5px;
+    color: #FFF;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:nth-child(1) {
+      background-color: #FF3030;
+      border: 1px solid #FF3030;
+    }
+
+    &:nth-child(2) {
+      background-color: #FF922E;
+      border: 1px solid #FF922E;
+    }
+
+    &:nth-child(3) {
+      background-color: #2FBE34;
+      border: 1px solid #2FBE34;
+    }
+  }
+
 `;
